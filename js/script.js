2c84e0810ee97e32499eacc33145b9672d882c30
CacingNakal <68537013+CacingNakal@users.noreply.github.com>
2021-08-01 12:32:22 +0700

    Update tanggal pasti

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function(e) {
         $('#ytvid').tubular(options);
     }
     // Set the date we're counting down to
-    var countDownDate = new Date("Jul 7, 2021 00:00:01").getTime();
+    var countDownDate = new Date("Aug 7, 2021 00:00:01").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
